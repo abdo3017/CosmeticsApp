337a9a80d83205b03c6b34c385e5e1e552b58a9d
abdo3017 <abdogazy14@gmail.com>
2024-03-29 14:26:46 +0200

    t

--- src/MyApp.Domain/Entities/Category.cs
+++ src/MyApp.Domain/Entities/Category.cs
@@ -13,7 +13,7 @@ public partial class Category : BaseEntity
     public string? Description { get; set; }
 
     public string? Icon { get; set; }
-    public bool IsSelected { get; set; }
+    public bool? IsSelected { get; set; }
 
     public int? ParentId { get; set; }
 
