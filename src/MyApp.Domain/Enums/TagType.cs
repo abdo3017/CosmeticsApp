7deda20f1fddc61f5ded3b20522b4fb28381579e
abdo3017 <abdogazy14@gmail.com>
2024-03-31 11:41:12 +0200

    s

---
+++ src/MyApp.Domain/Enums/TagType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Domain.Enums
+{
+    public enum TagType
+    {
+        MostPopular = 0,
+        Recent = 1,
+        Best = 2
+    }
+}
