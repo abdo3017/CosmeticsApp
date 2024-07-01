d890a602f5dbed9ff334a584ef0e7be32201f41d
abdo3017 <abdogazy14@gmail.com>
2024-07-01 23:03:57 +0300

    decrypting payment

--- src/MyApp.Application/Helpers/EncryptionHelper.cs
+++ src/MyApp.Application/Helpers/EncryptionHelper.cs
@@ -9,9 +9,8 @@ namespace MyApp.Application.Helpers
 {
     public static class EncryptionHelper
     {
-       public static string DecryptAES(string AuthorizationCode)
+       public static string DecryptAES(string encreptedValue)
         {
-
             string keyString = "13313586896631234900207000800912"; //replace with your key
             string ivString = "6896631234900212"; //replace with your iv
             byte[] key = Encoding.ASCII.GetBytes(keyString);
@@ -22,12 +21,12 @@ namespace MyApp.Application.Helpers
             {
                 rijndaelManaged.BlockSize = 128;
                 rijndaelManaged.KeySize = 256;
-                Span<byte> buffer = new Span<byte>(new byte[AuthorizationCode.Length]);
-                bool x = Convert.TryFromBase64String(AuthorizationCode, buffer, out int bytesParsed);
+                Span<byte> buffer = new Span<byte>(new byte[encreptedValue.Length]);
+                bool x = Convert.TryFromBase64String(encreptedValue, buffer, out int bytesParsed);
                 if (x)
                 {
                     using (var memoryStream =
-                      new MemoryStream(Convert.FromBase64String(AuthorizationCode)))
+                      new MemoryStream(Convert.FromBase64String(encreptedValue)))
                     using (var cryptoStream =
                            new CryptoStream(memoryStream,
                                rijndaelManaged.CreateDecryptor(key, iv),
@@ -36,8 +35,55 @@ namespace MyApp.Application.Helpers
                         return new StreamReader(cryptoStream).ReadToEnd();
                     }
                 }
-                return AuthorizationCode;
+                return encreptedValue;
             }
         }
+
+        public static Aes GenerateSymmetricKey()
+        {
+            Aes aes = Aes.Create();
+            aes.KeySize = 256; // 256-bit key for AES
+            string keyString = "13313586896631234900207000800912"; //replace with your key
+            string ivString = "6896631234900212"; //replace with your iv
+            byte[] key = Encoding.ASCII.GetBytes(keyString);
+            byte[] iv = Encoding.ASCII.GetBytes(ivString);
+            aes.Key = key;
+            aes.IV = iv;
+            return aes;
+        }
+
+        public static byte[] EncryptData(Aes aes, string data)
+        {
+            byte[] encrypted;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(data);
+                    }
+                    encrypted = ms.ToArray();
+                }
+            }
+            return encrypted;
+        }
+
+        public static string DecryptData(Aes aes, byte[] encryptedData)
+        {
+            string decrypted;
+            using (MemoryStream ms = new MemoryStream(encryptedData))
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader(cs))
+                    {
+                        decrypted = sr.ReadToEnd();
+                    }
+                }
+            }
+            return decrypted;
+        }
+
     }
 }
---
+++ src/MyApp.Application/Models/Mappers/PaymentInfoMapper.cs
@@ -0,0 +1,27 @@
+﻿using MyApp.Application.Helpers;
+using MyApp.Application.Models.DTOs;
+using MyApp.Domain.Entities;
+using MyApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Application.Models.Mappers
+{
+    public static class PaymentInfoMapper
+    {
+        public static PaymentInfoDto Decrypt(this PaymentInfoDto paymentInfo)
+        {
+            return new PaymentInfoDto
+            {
+                OrderId = paymentInfo.OrderId,
+                CardNumber = EncryptionHelper.DecryptData(EncryptionHelper.GenerateSymmetricKey(), Convert.FromBase64String(paymentInfo.CardNumber)),
+                ExpiryMonth = EncryptionHelper.DecryptData(EncryptionHelper.GenerateSymmetricKey(),Convert.FromBase64String(paymentInfo.ExpiryMonth)),
+                ExpiryYear = EncryptionHelper.DecryptData(EncryptionHelper.GenerateSymmetricKey(),Convert.FromBase64String(paymentInfo.ExpiryYear)),
+                SecurityCode = EncryptionHelper.DecryptData(EncryptionHelper.GenerateSymmetricKey(),Convert.FromBase64String(paymentInfo.SecurityCode)),
+            };
+        }
+    }
+}
